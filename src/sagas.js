8c11c8efe72b689abb3acc97fe7f08f576855bf3
lorenmh <neutronstar@gmail.com>
2018-02-08 15:12:55 -0800

    made dist binary, writing sagas

--- src/sagas.js
+++ src/sagas.js
@@ -6,20 +6,37 @@ import {
   INITIALIZE,
   SET_CHANNELS,
   CREATE_CHANNEL,
+
+  GET_CHANNELS_REQUEST,
   GET_CHANNELS_SUCCEEDED,
   GET_CHANNELS_FAILED
 } from './actions';
 
 import { getChannels } from './api';
 
-function* initialize() {
+function* channelsSaga() {
   try {
+    yield put(action(GET_CHANNELS_REQUEST));
     const channels = yield call(getChannels);
     yield put(action(GET_CHANNELS_SUCCEEDED));
     yield put(action(SET_CHANNELS, {channels}));
   } catch (e) {
     yield put(action(GET_CHANNELS_FAILED));
   }
+}
+
+function* initialize() {
+  yield* channelsSaga();
+}
+
+function* listen() {
+  try {
+
+  } catch (e) {
+  }
+}
+
+function* channelData() {
 }
 
 export default function* rootSaga() {
